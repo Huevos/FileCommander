df4424c80502bd4612ecbe30a32f58bf8a659a8d
Huevos <rowland@moralet.com>
2026-05-05 23:06:58 +0200

    Rename folder "plugin" > "src/FileCommander"

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ setup(name='enigma2-plugin-extensions-filecommander',
        version='2.13',
        description='Two-panels File manager.',
        packages=[pkg, 'Extensions.FileCommander.addons'],
-       package_dir={pkg: 'plugin'},
+       package_dir={pkg: 'FileCommander'},
        package_data={pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass, # for translation
       )
